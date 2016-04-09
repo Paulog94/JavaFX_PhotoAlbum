4c62e5261329c406dda1c572385c503002b1209a
Joshua Cross <jpeg9000@gmail.com>
2016-04-09 15:07:35 -0400

    thumbview controller

---
+++ PhotoAlbum53/src/photoAlbum/view/thumbViewControler.java
@@ -0,0 +1,72 @@
+package photoAlbum.view;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import photoAlbum.Model.User;
+
+public class thumbViewControler {
+	
+    private ArrayList<User> savedUsers;
+    @FXML private TextField txtAddUser;
+    @FXML private Button btnAddPhoto;
+    @FXML private Button btnREmPhoto;
+    @FXML private Button btnCapTag;
+    @FXML private Button btnSearch;
+    @FXML private Button btnMovePhoto;
+    @FXML private Button btnLogOut;
+    @FXML private Button btnExit;
+    @FXML private Button btnOpen;
+    
+    @FXML private ListView<User> UserList = new ListView<User>();
+
+    public thumbViewControler(){
+    }
+
+
+    
+    
+    public void Save(ArrayList<User> users){
+
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream("data/UserList.bin"));
+            oos.writeObject(users);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void LoadUserList(){
+
+        ObjectInputStream ois;
+        try {
+            ois = new ObjectInputStream(new FileInputStream("data/UserList.bin"));
+
+
+            ArrayList<User> UL;
+            try {
+                UL = (ArrayList<User>) ois.readObject();
+                savedUsers = UL;
+            } catch (ClassNotFoundException e) {
+                savedUsers = new ArrayList<User>();
+            }
+
+        } catch (IOException e) {
+            savedUsers = new ArrayList<User>();
+            return;
+        }
+    }
+
+
+}
