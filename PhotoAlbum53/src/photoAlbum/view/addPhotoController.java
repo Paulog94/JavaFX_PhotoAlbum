d5e38f060bfcb1b51e07d23368ba8fce5734a4df
Joshua Cross <jpeg9000@gmail.com>
2016-04-11 13:09:38 -0400

    add photo box and controller stubbed out.

---
+++ PhotoAlbum53/src/photoAlbum/view/addPhotoController.java
@@ -0,0 +1,18 @@
+package photoAlbum.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class addPhotoController {
+
+	@FXML private TextField path;
+	@FXML private TextField caption;
+	@FXML private Button save;
+	@FXML private Button cancel;
+	@FXML private Button files;
+
+
+
+
+}
